47c03dd5daba275726aa0d7964b251ed7537595c
Bondhon1 <bondhon0101@gmail.com>
2026-01-26 20:58:29 +0600

    feat: Update build configuration to integrate KSP and adjust compile SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.example.offlinemusicplayer"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.offlinemusicplayer"
@@ -51,7 +49,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    kapt(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
